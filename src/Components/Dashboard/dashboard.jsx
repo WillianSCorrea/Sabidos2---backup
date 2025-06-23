ce8bc5595d13dccd77b2d9db61a436ac84b96528
WillianSCorrea <willianwcorrea@gmail.com>
2025-06-23 16:59:21 -0300

    feat: Add SVG icons for study time and statistics in Dashboard component

--- src/Components/Dashboard/dashboard.jsx
+++ src/Components/Dashboard/dashboard.jsx
@@ -76,11 +76,13 @@ const Dashboard = () => {
           </div>
         </div>
 
+
         <div className="infos">
           <div className="tempo_estudo">
             <div className='contencao_svgte'>
               <div className='blcsvg_do_te'>
-                <img src='' className='' alt="" />
+                <img src='clock.svg' className='clock'>
+                </img>
               </div>
             </div>
             <div className='blc_do_te'>
@@ -89,27 +91,40 @@ const Dashboard = () => {
             </div>
           </div>
 
+
           <div className="estatisticas">
             <div className="itens_ferramentas">
-              <div className="top-square"></div>
+              <div className="top-square">
+                <img src='notas.svg' className='notas'>
+                </img>
+              </div>
               <div className="item-nome">Notas</div>
               <div className="item-valor">{totalResumos}</div>
             </div>
             <div className="itens_ferramentas">
-              <div className="top-square"></div>
+              <div className="top-square">
+                <img src='caarta.svg' className='cards'>
+                </img>
+              </div>
               <div className="item-nome">Eventos</div>
               <div className="item-valor">{totalEvents}</div>
             </div>
             <div className="itens_ferramentas">
-              <div className="top-square"></div>
+              <div className="top-square">
+                <img src='caarta.svg' className='cards'>
+                </img>
+              </div>
               <div className="item-nome">Cards</div>
               <div className="item-valor">{totalFlashcards}</div>
             </div>
+
           </div>
         </div>
       </div>
     </div>
+
   );
 };
 
 export default Dashboard;
+
